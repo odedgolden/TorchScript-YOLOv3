bf276ef9e6dd3c582051db775cff80ff284430cb
Oded Golden <odedgolden@gmail.com>
2020-04-15 14:23:55 +0300

    Removed YOLODummy

--- src/YOLOv3.py
+++ src/YOLOv3.py
@@ -13,7 +13,6 @@ class YOLOv3(YOLOModule):
     def __init__(self):
         super(YOLOv3, self).__init__()
         self.yolo_layer = YOLOLayer(anchors=[(2, 4), (6, 8)], num_classes=1)
-        # self.yolo_layer = YOLODummy(anchors=[(2, 4), (6, 8)], num_classes=1)
 
     def forward(self, x):
         x = self.yolo_layer(x)
